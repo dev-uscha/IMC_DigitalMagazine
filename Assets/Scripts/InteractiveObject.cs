6f9c2b98d3fb020462dca137805ae3c7ae0c0adf
dev-uscha <hanna.mielczarek@outlook.de>
2025-04-24 16:49:49 +0200

    Player Bewegbar

---
+++ Assets/Scripts/InteractiveObject.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class InteractiveObject : MonoBehaviour
+{
+    // Methode, die ausgeführt wird, wenn das Objekt interagiert wird
+    public void Interact()
+    {
+        Debug.Log("Das interaktive Objekt wurde aktiviert!");
+
+        // Beispiel: Hier kannst du Dinge tun, wie das Objekt öffnen oder animieren.
+        // Zum Beispiel:
+        // - Objekt verschieben
+        // - Animation abspielen
+        // - Objekt deaktivieren
+    }
+}
